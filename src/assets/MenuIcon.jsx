4bae6ea37c70365ecdf1327a25ae82aa0f56449f
pablo pelardas <pablo.pelardas@gmail.com>
2022-08-02 02:14:18 -0300

    feature:assets

---
+++ src/assets/MenuIcon.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const svg = <svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="Assets/Menu/Icon/Gray">
+              <g id="Menu">
+              <rect id="Rectangle 4 Copy 2" y="17.1428" width="28" height="2.85714" rx="1.42857" fill="#191919"/>
+              <rect id="Rectangle 4 Copy 4" y="8.57153" width="28" height="2.85714" rx="1.42857" fill="#191919"/>
+              <rect id="Rectangle 4 Copy 5" width="28" height="2.85714" rx="1.42857" fill="#191919"/>
+              </g>
+              </g>
+            </svg>
+
+const MenuIcon = () => {
+  return (
+    <span>{svg}</span>
+  )
+}
+
+export default MenuIcon
